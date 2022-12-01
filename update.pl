6bdab621031bb91635550a0cee10a8e23d3f7ef0
licong <licong@qianxin.com>
2022-12-01 18:09:30 +0800

    update

--- update.pl
+++ update.pl
@@ -35,7 +35,7 @@ for my $dir (@dirs) {
     printf $fd "<ul style=\"list-style: none;\">";
     for my $file (@files) {
         next if ( not -f $file );
-        my $title = sprintf "<li><a href=\"%s\">[%s]&nbsp&nbsp%s</a></li>", $file, get_date($file), gen_title($file);
+        my $title = sprintf "<li><a href=\"%s\">[%s]  %s</a></li>", $file, get_date($file), gen_title($file);
         print $fd $title;
     }
     printf $fd "</ul>";
